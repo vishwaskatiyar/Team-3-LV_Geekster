be872dcca729da61e700b56f86bcee85c1ef6f85
madhusudan9332 <madhusudangautam003@gmail.com>
2024-02-10 01:52:20 +0530

    modefied gifts-for-women page

--- madhusudan/images/gifts-for-women/big/renameSerialWise.bat
+++ madhusudan/images/gifts-for-women/big/renameSerialWise.bat
@@ -5,7 +5,7 @@ setlocal enabledelayedexpansion
 REM set "replace_word=new_word"
 for %%a in (*.png*) do (
     REM echo %%a >> data.txt
-    ren "%%a" small^-!i!.png
+    ren "%%a" big^-!i!.png
     REM !i! represent 1 2 3 4 ...
     REM type input.txt | sed "s/%search_word%/%replace_word%/g" > output.txt
     set /a i+=1
